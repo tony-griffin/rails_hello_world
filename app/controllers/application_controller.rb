85ba98d14b7a1c0705bf0afbff98b69f2b7f3b32
Tony Griffin <tony@madetech.com>
2022-10-20 10:11:45 +0000

    Initialise repo

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  
+  def hello
+    render html: "¡hello, world!"
+  end
+  
+  def goodbye
+    render html: "goodbye, world!"
+  end
+  
+  
+end
